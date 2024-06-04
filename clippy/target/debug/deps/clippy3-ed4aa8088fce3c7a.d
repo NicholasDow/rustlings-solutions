e9d3067a32d173539034dd813460b767a3e2c1c8
root <root@SwagBox>
2024-06-03 22:40:10 -0400

    init

---
+++ clippy/target/debug/deps/clippy3-ed4aa8088fce3c7a.d
@@ -0,0 +1,9 @@
+/root/rustlings/exercises/clippy/target/debug/deps/libclippy3-ed4aa8088fce3c7a.rmeta: clippy3.rs Cargo.toml
+
+/root/rustlings/exercises/clippy/target/debug/deps/clippy3-ed4aa8088fce3c7a.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
